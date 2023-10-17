dd17af8b33a56d8de6878e07aaa2fe8365eb5d34
Shanmukh <gangishanmukh@gmail.com>
2023-10-17 16:20:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from 'react';
+import AgeCalculator from './Agecalculator';
+
+function App() {
+  return (
+    <div className="App">
+      <AgeCalculator />
+    </div>
+  );
+}
+
+export default App;
